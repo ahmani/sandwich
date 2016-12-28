137bb68d6328597677f4f965b181a7db8de0f991
Antoine <agillet10@gmail.com>
2016-12-28 16:04:41 +0100

    ajout Commande.php et debut de la fonctionnalité 'nouvelle commande' (story n°9)

--- www/src/lbs/api/PublicController.php
+++ www/src/lbs/api/PublicController.php
@@ -4,6 +4,7 @@ namespace lbs\api;
 
 use lbs\common\model\Categorie;
 use lbs\common\model\Ingredient;
+use lbs\common\model\Commande;
 
 Class PublicController
 {
@@ -24,18 +25,13 @@ Class PublicController
 
 	public function getcategories($req,$rs,$args)
 	{
-<<<<<<< HEAD
 
 			    $cat = Categorie::select()->get();
 			    $rs = $rs->withStatus(200)
 			      ->withHeader('Content-Type', 'application/json;charset=utf8');
 			    $rs->getBody()->write($cat->toJson());
-=======
 				$col = array();
 			    $cat = Categorie::select()->get();
-			    $rs = $rs->withStatus(200)
-			      		 ->withHeader('Content-Type', 'application/json;charset=utf8');
-
 			    $cats = json_decode($cat->toJson());
 
 			    foreach ($cats as $cat) {
@@ -44,7 +40,7 @@ Class PublicController
 			      					  'links' => ['self' =>
 			      					  ['href' => $this->cont['router']->pathFor('categorie',['id' => $cat->id])]]]);
 			    }
-			    
+
 			    $rs->getBody()->write(json_encode($col));
 	}
 
@@ -52,7 +48,6 @@ Class PublicController
 	public function getIngredientByCategorie($req,$rs,$args)
 	{
 		die('here');
->>>>>>> de426c0fc65fa95c0eb75b15307199f4c0665d07
 	}
 
 	public function getingredient($req,$rs,$args)
@@ -69,4 +64,7 @@ Class PublicController
 		}
 	}
 
+	public function createCommande($req, $rs, $args){
+		
+	}
 }
