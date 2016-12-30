a2da5885492fcf66b007ed2bccc10a8e1f096726
Antoine <agillet10@gmail.com>
2016-12-30 18:42:50 +0100

    requete SQL pour l'ajout de le table commande dans la BD

---
+++ update_bd_commande.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Commande` (
+  `token` int(32) NOT NULL,
+  `etat` enum('created','paid','progress','ready','delivered') NOT NULL DEFAULT 'created',
+  `nom_client` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `date` date NOT NULL
+);
