c359ebcb54829fcdfa8a745394b0003d0f4e235a
Antoine <agillet10@gmail.com>
2016-12-30 19:28:36 +0100

    pitite modif de la requete

--- update_bd_commande.sql
+++ update_bd_commande.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Commande` (
+CREATE TABLE `commande` (
   `token` int(32) NOT NULL,
   `etat` enum('created','paid','progress','ready','delivered') NOT NULL DEFAULT 'created',
   `nom_client` varchar(64) NOT NULL,
