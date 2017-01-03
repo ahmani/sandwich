f052e464e35543f03d2e1710c1af01b6ce35bbcb
ahmani <ikram.ahmani@gmail.com>
2017-01-03 17:57:20 +0100

    Ajouter id et montant de la commande

--- update_bd_commande.sql
+++ update_bd_commande.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `commande` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `token` int(32) NOT NULL,
   `etat` enum('created','paid','progress','ready','delivered') NOT NULL DEFAULT 'created',
   `nom_client` varchar(64) NOT NULL,
   `email` varchar(64) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `montant` float(32) NOT NULL,
 );
