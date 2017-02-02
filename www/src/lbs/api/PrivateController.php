7047e88356b75836665da91d8792be7ebc287940
ahmani <ikram.ahmani@gmail.com>
2017-02-02 10:34:24 +0100

    private

--- www/src/lbs/api/PrivateController.php
+++ www/src/lbs/api/PrivateController.php
@@ -21,15 +21,44 @@ Class PrivateController
 
 	public function getcommandes($req,$rs,$args)
 	{
-		$cat = Commande::select()->get();
+		$cat = Commande::select()->orderBy('date_retrait','DESC')->orderBy('date','DESC')->get();
+		$commande_details = array();
 		$rs = $rs->withStatus(200)
 			->withHeader('Content-Type', 'application/json;charset=utf8');
-		$col = array();
 		$commandes = json_decode($cat->toJson());
 
 		foreach ($commandes as $value) {
 			$tab = GetSandwichsByCommande($value->id);
+			$commande_details[] = array("Nom du client" => $value->nom_client,
+						   "Email" => $value->email,
+						   "Date de création" => $value->date,
+						   "Date de retrait" => $value->date_retrait,
+						   "Etat" => $value->etat,
+						   "Sandwichs" => $tab);
 		}
+		$rs = $rs->withJson($commande_details, 200);
+		return $rs;
+
+	}
+
+	public function getCommandeDetail($req,$resp,$args)
+	{
+		if($args['id'])
+			{
+				$commande = Commande::where('id', '=', $args['id'])->firstOrFail();
+			}
+		else
+			{
+				return json_error($rs,500,"Id commande required");
+			}
+
+			$tab = GetSandwichsByCommande($value->id);
+			$commande_details[] = array("Nom du client" => $commande->nom_client,
+						   "Email" => $commande->email,
+						   "Date de création" => $commande->date,
+						   "Date de retrait" => $commande->date_retrait,
+						   "Etat" => $commande->etat,
+						   "Sandwichs" => $tab);
 
 	}
 
