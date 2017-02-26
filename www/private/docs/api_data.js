ec6fbdee06d60628ce9d606a39eb5b9715c64bfa
Antoine <agillet10@gmail.com>
2017-02-26 22:24:07 +0100

    doc

--- www/private/docs/api_data.js
+++ www/private/docs/api_data.js
@@ -94,5 +94,53 @@ define({ "api": [
     },
     "filename": "./api.php",
     "groupTitle": "Commandes"
+  },
+  {
+    "group": "Commandes",
+    "name": "getCommandes",
+    "version": "0.1.0",
+    "type": "get",
+    "url": "/commande/id",
+    "title": "Renvoie la liste complète des commandes , triée par date de livraison et ordre de création",
+    "description": "<p>Recuperer la liste complète des commandes , triée par date de livraison et ordre de création</p>",
+    "success": {
+      "examples": [
+        {
+          "title": "exemple de réponse en cas de succès",
+          "content": "[\n   {\n       \"Nom du client\": \"ikram\",\n       \"Email\": \"ikram.ahmani@gmail.com\",\n       \"Date de création\": \"2017-02-25 00:00:00\",\n       \"Date de retrait\": \"2017-02-22 18:25:00\",\n       \"Etat\": \"progress\",\n       \"Sandwichs\": [\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": []\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": []\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": []\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": {\n                   \"salades\": \"laitue\",\n                   \"crudités\": \"carottes\",\n                   \"viandes\": \"blanc de poulet\",\n                   \"Fromages\": \"chèvre frais\"\n               }\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": {\n                   \"salades\": \"laitue\",\n                   \"crudités\": \"carottes\",\n                   \"viandes\": \"blanc de poulet\",\n                   \"Fromages\": \"chèvre frais\"\n               }\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": {\n                   \"salades\": \"laitue\",\n                   \"crudités\": \"carottes\",\n                   \"viandes\": \"blanc de poulet\",\n                   \"Fromages\": \"chèvre frais\"\n               }\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": {\n                   \"salades\": \"laitue\",\n                   \"crudités\": \"carottes\",\n                   \"viandes\": \"blanc de poulet\",\n                   \"Fromages\": \"chèvre frais\"\n               }\n           },\n           {\n               \"Taille\": \"petite faim\",\n               \"Type\": \"blanc\",\n               \"ingredients\": {\n                   \"salades\": \"laitue\",\n                   \"crudités\": \"carottes\",\n                   \"viandes\": \"blanc de poulet\",\n                   \"Fromages\": \"chèvre frais\",\n                   \"Sauces\": \"vinaigrette huile d'olive\"\n               }\n           }\n       ]\n   }\n]",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./api.php",
+    "groupTitle": "Commandes"
+  },
+  {
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "optional": false,
+            "field": "varname1",
+            "description": "<p>No type.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "varname2",
+            "description": "<p>With type.</p>"
+          }
+        ]
+      }
+    },
+    "type": "",
+    "url": "",
+    "version": "0.0.0",
+    "filename": "./docs/main.js",
+    "group": "_home_antoine_Documents_sandwich_www_private_docs_main_js",
+    "groupTitle": "_home_antoine_Documents_sandwich_www_private_docs_main_js",
+    "name": ""
   }
 ] });
